bacc88c990d89da035afa3f4d177c5b8a6c3ea61
Diego Acosta <diegoacosta211@gmail.com>
2021-03-24 00:03:00 -0300

    useFetch custom hook and add fetchReducer to handle fetching data state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,10 +7,68 @@ import CharacterList from './CharacterList';
 
 import dummyData from './dummy-data';
 
+import endpoint from './endpoint';
+
 import './styles.scss';
 
+const initialState = {
+  result: null,
+  loading: true,
+  error: null
+}
+
+const fetchReducer = (state = initialState, { type, payload }) => {
+  console.log(type);
+  if (type === 'FETCH_START') {
+    return {
+      result: null,
+      loading: true,
+      error: null
+    }
+  }
+
+  if (type === 'FETCH_SUCCESS') {
+    return {
+      result: payload,
+      loading: false,
+      error: null
+    }
+  }
+
+  if (type === 'FETCH_ERROR') {
+    return {
+      result: null,
+      loading: false,
+      error: payload
+    }
+  }
+
+  return state;
+}
+
+const useFetch = url => {
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_START' });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: 'FETCH_ERROR', payload: err });
+      });
+  }, [url]);
+
+  return [state.result, state.loading, state.error];
+
+}
+
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(`${endpoint}/characters`);
+  const characters = response?.characters || [];
 
   return (
     <div className="Application">
@@ -19,7 +77,14 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {
+            loading ?
+              <div>Loading...</div> :
+              <CharacterList characters={characters} />
+          }
+          {
+            error && <div className="error">{`There was an error ${error.message}`}</div>
+          }
         </section>
       </main>
     </div>
